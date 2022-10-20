bca5d29b42f726a157e952e777e2a4ff15a99d8f
Gunichi <gunichi297@gmail.com>
2022-10-19 22:45:46 -0300

    Exercicio 1157

--- Exercícios/1113.py
+++
@@ -1,9 +0,0 @@
-while True:
-    valores = [int(i) for i in input().split()]
-    x, y = valores
-    if x == y:
-        break
-    elif x > y:
-        print('Decrescente')
-    else:
-        print('Crescente')
